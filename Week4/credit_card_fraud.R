483f16e8bed90e2a2639532ae2c755dd13cfd19d
gitcnk <58092026+gitcnk@users.noreply.github.com>
2026-03-27 22:04:47 -0400

    Create credit_card_fraud.R

---
+++ Week4/credit_card_fraud.R
@@ -0,0 +1,7 @@
+library(tidyverse)
+library(rpart)
+library(sparkline)
+library(visNetwork)
+library(randomForest)
+
+credit_data = read.csv('https://raw.githubusercontent.com/gitcnk/Pioneer_Spring2026/refs/heads/main/Week4/credit_default.csv')
