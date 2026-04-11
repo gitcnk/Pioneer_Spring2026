3023608ce4aff82313916faf9f50b013b2a358c0
gitcnk <58092026+gitcnk@users.noreply.github.com>
2026-04-11 14:27:49 -0400

    Update HW5.R

--- Week5/HW5.R
+++ Week5/HW5.R
@@ -12,11 +12,16 @@ train_ids = sample(ix, size = 500)
 train_set = breast_cancer[ train_ids, ]  # This has 500 cases
 test_set = breast_cancer[ -train_ids, ]  # This has 69 cases
  
-## For the following models use the training dataset
+# For the following models use the training dataset
+
 ### 1. Fit a tree model 
 
 ### 2. Fit a random forest model
 
 ### 3. Fit a boosting model
+
+
+##------------------------------------
+
 
 ### 4. Evaluate the accuracy of these 3 models using the test set
